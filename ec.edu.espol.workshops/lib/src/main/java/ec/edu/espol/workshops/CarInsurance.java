0de2118f072382022f7b42ee898a9278c4307dd7
jlvalenz <jlvalenz@espol.edu.ec>
2021-10-15 17:18:20 -0500

    Creation of the project and of the class CarInsurance

---
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -0,0 +1,13 @@
+package ec.edu.espol.workshops;
+
+public class CarInsurance {
+	int age;
+	char sex;
+	boolean married; // 1 is married else 0
+	double basePrice = 500;
+	
+	public static void main() {
+		System.out.println("Larry");
+	}
+}
+
