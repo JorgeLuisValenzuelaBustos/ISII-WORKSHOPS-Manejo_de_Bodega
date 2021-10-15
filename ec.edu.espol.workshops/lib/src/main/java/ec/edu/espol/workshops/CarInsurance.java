36a90a3167ca84448ddfd873ac87963781694a7d
josue-caballero <josue.caballero.macias@outlook.com>
2021-10-15 18:10:17 -0500

    Add getters for all properties of class CarInsurance

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -1,13 +1,34 @@
+
 package ec.edu.espol.workshops;
 
 public class CarInsurance {
+	
 	int age;
 	char sex;
 	boolean married; // 1 is married else 0
 	double basePrice = 500;
 	
-	public static void main() {
-		System.out.println("Larry");
+	public boolean setMarried(boolean married) { return this.married = married; }
+	
+	public void setAge(int age) { 
+		
+		if(age > 18) {
+			this.age = age; }
+		
+	} 
+	
+	public void setBasePrice(double basePrice) { 
+		
+		if(basePrice > 0) {
+			this.basePrice = basePrice; }
+		
 	}
-}
+	
+	public void setSex(char sex) {
+		
+		if( (sex == 'H') || (sex == 'M') ) {
+			this.sex = sex; }
+		
+	}
 
+}
