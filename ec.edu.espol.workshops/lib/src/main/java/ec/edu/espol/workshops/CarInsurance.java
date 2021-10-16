962555871886e4251c730134ae8c7907a49c7cec
MelinaMacias <mmmacias@espol.edu.ec>
2021-10-15 19:47:04 -0500

    Created the setters methods and method to calculate the premium value

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -8,6 +8,23 @@ public class CarInsurance {
 	boolean married; // 1 is married else 0
 	double basePrice = 500;
 	
+	
+	public int getAge() {
+		return age;
+	}
+
+	public char getSex() {
+		return sex;
+	}
+
+	public boolean isMarried() {
+		return married;
+	}
+
+	public double getBasePrice() {
+		return basePrice;
+	}
+
 	public boolean setMarried(boolean married) { return this.married = married; }
 	
 	public void setAge(int age) { 
@@ -30,5 +47,55 @@ public class CarInsurance {
 			this.sex = sex; }
 		
 	}
+	
+	
+	public double calculatePremium() {
+
+		if((this.sex== 'M') && (this.married== false) && (this.age<25)) {
+			this.basePrice= +1500;
+			return this.basePrice;
+			
+		}
+		
+		else if((this.sex== 'F') || (this.married== true)) {
+			this.basePrice=-200;
+			return this.basePrice;
+			
+		}
+		
+		else if((this.age >=45) && (this.age < 65 )) {
+			this.basePrice=-100;
+			return this.basePrice;
+			
+		}
+		
+		else {
+			return -1;
+		}
+		
+		
+	}
+	
+	public void sellInsurance() {
+		
+		if((this.age >80)||(generateLicense() == null)) {
+			 System.out.print("Doesn't meet the requirements");
+		}
+		
+	}
+	
+	
+	public String generateLicense() {
+		
+		 int license = 0;
+		 license = (int)((Math.random() * 900000000)+10000000);
+		 
+		 return String.valueOf(license);
+		
+	}
+	
+	
+	
+	
 
 }
