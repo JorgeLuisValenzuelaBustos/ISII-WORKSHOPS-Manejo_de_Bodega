af2e0d2347898e5afe3f0c8f5764c48dd30e97c3
MelinaMacias <mmmacias@espol.edu.ec>
2021-10-22 20:51:32 -0500

    Solved CheckStyle Violations

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -1,101 +1,82 @@
 
 package ec.edu.espol.workshops;
 
-public class CarInsurance {
+public class CarInsurance{
 	
-	int age;
-	char sex;
-	boolean married; // 1 is married else 0
-	double basePrice = 500;
-	
-	
-	public int getAge() {
+  int age;
+  char sex;
+  boolean married; // 1 is married else 0
+  double basePrice = 500;
+
+  public int getAge() {
+
 		return age;
-	}
 
-	public char getSex() {
-		return sex;
-	}
+  }
 
-	public boolean isMarried() {
-		return married;
-	}
+  public char getSex() {
 
-	public double getBasePrice() {
-		return basePrice;
-	}
+	return sex;
+	
+}
 
-	public boolean setMarried(boolean married) { return this.married = married; }
-	
-	public void setAge(int age) { 
-		
-		if(age > 18) {
-			this.age = age; }
-		
-	} 
-	
-	public void setBasePrice(double basePrice) { 
-		
-		if(basePrice > 0) {
-			this.basePrice = basePrice; }
-		
-	}
-	
-	public void setSex(char sex) {
-		
-		if( (sex == 'H') || (sex == 'M') ) {
-			this.sex = sex; }
-		
-	}
-	
-	
-	public double calculatePremium() {
+  public boolean isMarried() {
 
-		if((this.sex== 'M') && (this.married== false) && (this.age<25)) {
-			this.basePrice= +1500;
+  return married;
+
+  }
+
+  public double getBasePrice() {
+	return basePrice;
+  }
+
+  public boolean setMarried(boolean married) {
+	return this.married = married;
+  }
+
+  public void setAge(int age) {
+
+	if(age > 18) {
+		this.age = age;
+	}
+  }
+	
+  public void setBasePrice(double basePrice) {
+	if (basePrice > 0) {
+		this.basePrice = basePrice;
+    }
+  }
+	
+  public void setSex(char sex) {
+	if ((sex == 'H') || (sex == 'M')) {
+		this.sex = sex;
+	}
+  }
+
+  public double calculatePremium() {
+	if (( this.sex == 'M' ) && (this.married== false) && (this.age<25)) {
+		this.basePrice= +1500;
 			return this.basePrice;
-			
-		}
-		
-		else if((this.sex== 'F') || (this.married== true)) {
-			this.basePrice=-200;
-			return this.basePrice;
-			
-		}
-		
-		else if((this.age >=45) && (this.age < 65 )) {
-			this.basePrice=-100;
-			return this.basePrice;
-			
-		}
-		
-		else {
-			return -1;
-		}
-		
-		
-	}
-	
-	public void sellInsurance() {
-		
-		if((this.age >80)||(generateLicense() == null)) {
+	} else if ((this.sex == 'F') || (this.married== true)) {
+		this.basePrice=-200;
+		return this.basePrice;
+  }
+	else if ((this.age >=45) && (this.age < 65 )) {
+		this.basePrice=-100;
+		return this.basePrice;
+	} else {
+			return -1; }
+  }
+
+  public void sellInsurance() {
+		if ((this.age > 80) || (generateLicense() == null)) {
 			 System.out.print("Doesn't meet the requirements");
-		}
-		
 	}
-	
+  }
 	
 	public String generateLicense() {
-		
 		 int license = 0;
-		 license = (int)((Math.random() * 900000000)+10000000);
-		 
+		 license = (int) ((Math.random() * 900000000) + 10000000);
 		 return String.valueOf(license);
-		
 	}
-	
-	
-	
-	
-
 }
