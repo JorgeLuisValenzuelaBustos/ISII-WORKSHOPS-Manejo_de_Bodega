59e0ccacfccb2065b19f76fcb5f45b73b1d71c1c
MelinaMacias <mmmacias@espol.edu.ec>
2022-01-20 20:48:35 -0500

    Test Cases generated for new rules

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsuranceTest.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsuranceTest.java
@@ -23,5 +23,20 @@ public class CarInsuranceTest {
 		CarInsurance insurance = new CarInsurance(30,'F', false);
 		assertEquals(300,insurance.calculatePremium(),0);
 	}
-
+	
+	@Test
+	public void testCalculatePremiumMarried() {
+		CarInsurance insurance = new CarInsurance(40,'M', true);
+		assertEquals(400,insurance.calculatePremium(),0);
+	}
+	
+	@Test
+	public void testCalculatePremiumFemaleSingle() {
+		CarInsurance insurance = new CarInsurance(70,'F', false);
+		assertEquals(450,insurance.calculatePremium(),0);
+	}
+	
+	
+	
+	
 }
